179774b9a96bce5d6ca4e8588bd580cc234a495d
Tomycat <tangminghong.tom@gmail.com>
2024-08-04 22:10:11 +0800

    setup mod

---
+++ Alchemic-Realms-The-Chaos-Frontier-fabric/src/client/java/org/system/alchemic_realms_the_chaos_fronfier/client/Alchemic_realms_the_chaos_fronfierClient.java
@@ -0,0 +1,10 @@
+package org.system.alchemic_realms_the_chaos_fronfier.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Alchemic_realms_the_chaos_fronfierClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ Alchemic-Realms-The-Chaos-Frontier-fabric/src/main/java/org/system/alchemic_realms_the_chaos_fronfier/Alchemic_realms_the_chaos_fronfier.java
@@ -0,0 +1,10 @@
+package org.system.alchemic_realms_the_chaos_fronfier;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Alchemic_realms_the_chaos_fronfier implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
